3518463ed70f11ba926498c3172e468838ba63b2
camilamdz26 <camila_mendez@avenues-ny.org>
2024-12-06 22:34:14 -0500

    Update TELEOP.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TELEOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TELEOP.java
@@ -2,10 +2,11 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-//THIS INCLUDES NO SERVO CODE YET. ONLY LINEARSLIDES AND WHEELS. 
 
 @TeleOp(
         name = "TELEOP"
@@ -17,8 +18,12 @@ public class TELEOP extends LinearOpMode {
     DcMotor FL;
     DcMotor LSR;
     DcMotor LSL; //NAME THE LINEARSLIDE MOTORS LSL AND LSR
+    DcMotor AM; //linear slides for arm
     double r;
-    int maxExtension = 2200; //CHANGE THSI9 VALUE BASED ON THE MAX HEIGHT OF THE LS
+    int maxExtension = 300; //CHANGE THSI9 VALUE BASED ON THE MAX HEIGHT OF THE LS
+    int armExtension = 2200; //chnage this value based on the max length of the arm slides
+    CRServo RL;
+    Servo IR;
 
     public void runOpMode() {
 
@@ -27,9 +32,15 @@ public class TELEOP extends LinearOpMode {
         BR = hardwareMap.get(DcMotor.class, "BR"); // RD to BR
         BR.setDirection(Direction.REVERSE);
         FL = hardwareMap.get(DcMotor.class, "FL"); // LU to FL
-        FR = hardwareMap.get(DcMotor.class, "FR"); // RU to FR
+        FR = hardwareMap.get(DcMotor.class, "FR"); // RU to FR*/
         LSR = hardwareMap.get(DcMotor.class, "LSR");
         LSL = hardwareMap.get(DcMotor.class, "LSL");
+        AM = hardwareMap.get(DcMotor.class, "AM");
+        IR = hardwareMap.get(Servo.class, "IR");
+        RL = hardwareMap.get(CRServo.class, "RL");*/
+
+        //roller = continuous
+        //intake rotator = 270 degrees
 
         LSR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         LSL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -58,32 +69,73 @@ public class TELEOP extends LinearOpMode {
         }
 
         telemetry.addData(">", "Done");
-        telemetry.update();
+        telemetry.update();*/
 
 //I DON'T KNOW IF THIS WILL WORK, WE HAVE TO CHECK
-        if (LSR.getCurrentPosition() < maxExtension && LSL.getCurrentPosition() < maxExtension) { //if it is not already stretched
-            if (gamepad1.y) {
-                telemetry.update(); //if gamepad is pressed, turn on motors
-                LSR.setPower(1);
-                LSL.setPower(1);
-            } else {
-                LSR.setPower(0);
-                LSL.setPower(0); //if it isn't, turn off
-            }
-        } else {
-            LSR.setPower(0);
-            LSL.setPower(0); //if it is at max position, set power to 0
-        }
-        if (LSR.getCurrentPosition() > 0 && LSL.getCurrentPosition() > 0) { //if they are not at position 0
-            if (gamepad1.a) {
-                LSR.setPower(-0.5); //if you press a, will retract the slides.
-                LSL.setPower(-0.5);
-            } else {
-                LSR.setPower(0); //if it isn't, turn off
-                LSL.setPower(0);
+            if (LSR.getCurrentPosition() < maxExtension && LSL.getCurrentPosition() < maxExtension) { //if it is not already stretched
+                if (gamepad1.dpad_up) {
+                    telemetry.update(); //if gamepad is pressed, turn on motors
+                    LSR.setPower(1);
+                    LSL.setPower(1);
+                } else {
+                    if (LSR.getCurrentPosition() != 0) {
+                        LSR.setPower(0.05);
+                        LSL.setPower(0.05);
+                    } else {
+                        LSR.setPower(0);
+                        LSL.setPower(0);//if it isn't, turn off
+                        LSR.setPower(0);
+                        LSL.setPower(0); //if it is at max position, set power to 0
+                    }
+                    if (LSR.getCurrentPosition() > 0 && LSL.getCurrentPosition() > 0) { //if they are not at position 0
+                        if (gamepad1.dpad_down) {
+                            LSR.setPower(-0.5); //if you press a, will retract the slides.
+                            LSL.setPower(-0.5);
+                        } else {
+                            LSR.setPower(0); //if it isn't, turn off
+                            LSL.setPower(0);
+                        }
+
+                    }
+                }
+
+                //horizontal linear slides <-x b->
+                if (AM.getCurrentPosition() < armExtension) {
+                    if (gamepad1.dpad_right) {
+                        AM.setPower(1);
+                    } else {
+                        if (AM.getCurrentPosition() != 0) {
+                            AM.setPower(0.05);
+                        } else {
+                            AM.setPower(0);
+                        }
+                    }
+                }
+                if (AM.getCurrentPosition() > 0) {
+                    if (gamepad1.dpad_left) {
+                        AM.setPower(-0.5);
+                    } else {
+                        AM.setPower(0.05);
+                    }
+                }
+
+                if (gamepad1.b) {
+                    IR.setPosition(1);
+                }
+
+                if (gamepad1.x) {
+                    IR.setPosition(0);
+                }
+
+                if (gamepad1.y) {
+                    RL.setPower(1);
+                }
+                if (gamepad1.a) {
+                    RL.setPower(0);
+                }
             }
 
+            }
         }
     }
 }
-
